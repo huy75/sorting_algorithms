a00cf6f33c880c590a51444741bc95fd35a9e2e6
huy75 <huy.nguyen66@gmail.com>
2021-01-28 14:29:33 +0000

    feat skeleton of bubble & insertion sorts

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+/**
+ * swapp - Swaps specified array elements then prints array
+ * @array: Array to swap elements
+ * @size: Size of array
+ * @i: First index to swap
+ * @j: Second index to swap
+ */
+void swapp(int *array, size_t size, int i, int j)
+{
+	print_array(array, size);
+}
+
+/**
+ * bubble_sort - sorts an array using bubble sort
+ * @array: array of ints
+ * @size: number of elements in array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+/**
+ * swapp_list - Swaps linked list nodes then prints list
+ * @list: Linked list to swap
+ * @n1: First node
+ * @n2: Second node
+ */
+void swapp_list(listint_t **list, listint_t *n1, listint_t *n2)
+{
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: Linked list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+}
