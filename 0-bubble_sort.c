1aa3393fd58debb37f24fba6ea38696bdebc0595
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-28 16:20:57 +0000

    1st version

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdbool.h>
 
 /**
  * swapp - Swaps specified array elements then prints array
@@ -7,10 +8,20 @@
  * @i: First index to swap
  * @j: Second index to swap
  */
-void swapp(int *array, size_t size, int i, int j)
+
+/**
+void swap(int *array, size_t size, int i, int j)
 {
+	int tmp;
+
+	tmp = array[j];
+	array[j] = array[i];
+	array[i] = tmp;
+
 	print_array(array, size);
 }
+
+*/
 
 /**
  * bubble_sort - sorts an array using bubble sort
@@ -18,7 +29,61 @@ void swapp(int *array, size_t size, int i, int j)
  * @size: number of elements in array
  * Return: void
  */
+/* Output seems ok*/
+
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int tmp;
+
+	if (array && size > 1 )
+	{
+		for (i = 0; i < size; i++)
+		{
+			/* size - 1 to avoid largest elemnt in the final place */
+			/* so that no need to double check */
+			for (j = 0; j < size - 1; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					tmp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = tmp;
+					print_array(array, size);
+				}
+			}
+		}
+	}
+	else
+		return;
 
 }
+
+
+
+/*First version of the code
+ * output? needs improvement
+ */
+
+/**
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+
+	while (array)
+	{
+		for (i = 0; i < size -1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(array, size, i, i + 1);
+				print_array(array, size);
+			}
+		}
+	}
+	return;
+}
+
+
+*/
