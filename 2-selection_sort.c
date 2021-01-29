c083ef68a2ea220de42c5e86f84499e3164089cc
Battsetseg20 <tsetseg.yo@gmail.com>
2021-01-29 09:31:11 +0000

    add task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,4 +6,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
+
+	unsigned int i,j,tmp, min;
+
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+
 }
