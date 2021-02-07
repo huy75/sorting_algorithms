efd11362524d963fe4a57bf0874a82dfede99322
huy75 <huy.nguyen66@gmail.com>
2021-02-07 15:31:57 +0000

    fix

--- 105-main.c
+++ 105-main.c
@@ -9,10 +9,11 @@
  */
 int main(void)
 {
-	int array[] = {1, 22, 20, 21, 2, 31, 32, 34, 41, 39, 47, 79, 68, 62, 84, 87, 98, 92, 95, 91};
 /*
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	int array[] = {1};
 */
+	int array[] = {1, 2, 20, 21, 22, 31, 32, 34, 39, 41, 47, 62, 68, 79, 84, 87, 91, 92, 95, 98};
+
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
@@ -20,5 +21,6 @@ int main(void)
 	radix_sort(array, n);
 	printf("\n");
 	print_array(array, n);
+
 	return (0);
 }
